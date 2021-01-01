49c0b5e9852c213cdc2516595b2760eb34fd12d7
Leo-paul <leopaulmartin@hotmail.fr>
2021-01-01 01:48:50 +0100

    use async call API and Nextjs.Head API

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import Head from "next/head";
+
 function Home() {
-  return <div>Welcome, Explorer!</div>;
+  return (
+    <>
+      <Head>
+        <title>My Next.js Site!</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-with" />
+      </Head>
+      <div>Welcome, Explorer!</div>
+    </>
+  );
 }
 
 export default Home;
